dc0cb471e2dbd4b816f69d79f7c20f882acf5490
Ferry Abt <ferry.abt@knime.com>
2017-11-16 12:54:22 +0100

    AP-8014: pt1: Move Setup dialog inside
    AP-8014 (Harmonize Wrapped Metanode configuration options)

--- org.knime.workbench.editor/src/org/knime/workbench/editor2/WorkflowContextMenuProvider.java
+++ org.knime.workbench.editor/src/org/knime/workbench/editor2/WorkflowContextMenuProvider.java
@@ -119,7 +119,6 @@ import org.knime.workbench.editor2.actions.SaveAsSubNodeTemplateAction;
 import org.knime.workbench.editor2.actions.SelectLoopAction;
 import org.knime.workbench.editor2.actions.SetNodeDescriptionAction;
 import org.knime.workbench.editor2.actions.StepLoopAction;
-import org.knime.workbench.editor2.actions.SubNodeReconfigureAction;
 import org.knime.workbench.editor2.actions.ToggleFlowVarPortsAction;
 import org.knime.workbench.editor2.editparts.NodeContainerEditPart;
 import org.knime.workbench.editor2.editparts.WorkflowInPortBarEditPart;
@@ -406,11 +405,6 @@ public class WorkflowContextMenuProvider extends ContextMenuProvider {
 
                     // EXPAND SUBNODE
                     action = m_actionRegistry.getAction(ExpandSubNodeAction.ID);
-                    subnodeMenuMgr.appendToGroup(GROUP_SUBNODE, action);
-                    ((AbstractNodeAction)action).update();
-
-                    // RECONFIGURE SUBNODE
-                    action = m_actionRegistry.getAction(SubNodeReconfigureAction.ID);
                     subnodeMenuMgr.appendToGroup(GROUP_SUBNODE, action);
                     ((AbstractNodeAction)action).update();
 
